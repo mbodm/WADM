ea314cd082ea86b8167bff9ebda137fee047d76a
MBODM <webmaster@mbodm.com>
2019-07-07 06:12:39 +0200

    Reverted all changes in master branch, caused by the "https://wow.curseforge.com/projects"-Variant. Switched back to the "https://www.curseforge.com/wow/addons"-Variant instead. The transition from curse.com to curseforge.com was a bit weird, back in the days, and resulted in these strange code changes. Changed MBODM.WOW.CurseParser to actual "https://www.curseforge.com/wow/addons" website design of July 2019. Version of MBODM.WOW.CurseParser stays at 1.0.0.12, since mentioned variant was never released. Version of MBODM.WADM.UI stays at 2.0.0.8, since mentioned variant was never released. This version is the official version 2.0.0.8, completelly based on the 2.0.0.7 release version. No other changes were done, besides the mentioned MBODM.WOW.CurseParser change and the both version increases.

--- WADM/MBODM.WADM/MBODM.WOW.CurseParser/Sourcecode/Public/Classes/CurseParser.cs
+++ WADM/MBODM.WADM/MBODM.WOW.CurseParser/Sourcecode/Public/Classes/CurseParser.cs
@@ -10,7 +10,7 @@ namespace MBODM.WOW
 {
     public sealed class CurseParser : ICurseParser
     {
-        private const string CurseAddonUrl = "https://wow.curseforge.com/projects/";
+        private const string CurseAddonUrl = "https://www.curseforge.com/wow/addons/";
         private const string ArgumentNullOrEmptyMessage = "Argument is null or empty.";
         private const string InvalidCurseAddonUrlMessage = "The url is not a valid curse addon url.";
 
@@ -80,15 +80,33 @@ namespace MBODM.WOW
 
                 using (var httpClient = new HttpClient(httpClientHandler))
                 {
-                    var url = CurseAddonUrl + addonName + "/files/latest";
+                    var url1 = CurseAddonUrl + addonName + "/download";
 
-                    using (var request = new HttpRequestMessage(HttpMethod.Get, url))
+                    using (var request1 = new HttpRequestMessage(HttpMethod.Get, url1))
                     {
-                        var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                        var response1 = await httpClient.SendAsync(request1, cancellationToken).ConfigureAwait(false);
 
-                        var downloadUrl = response.Headers.Location.ToString();
+                        var content = await response1.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-                        return downloadUrl;
+                        var url2 = content.
+                            Split(new string[] { "<p class=\"text-sm\">" }, StringSplitOptions.RemoveEmptyEntries).
+                            Last().
+                            Split(new string[] { ">here</a>" }, StringSplitOptions.RemoveEmptyEntries).
+                            First().
+                            Split(new string[] { "<a href=" }, StringSplitOptions.RemoveEmptyEntries).
+                            Last().
+                            Trim().
+                            Trim('"').
+                            Replace("/wow/addons/", CurseAddonUrl);
+
+                        using (var request2 = new HttpRequestMessage(HttpMethod.Get, url2))
+                        {
+                            var response2 = await httpClient.SendAsync(request2, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+
+                            var downloadUrl = response2.Headers.Location.ToString();
+
+                            return downloadUrl;
+                        }
                     }
                 }
             }
