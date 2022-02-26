f8e85611258a4b2d31618ae8abb5857b427d353e
MBODM <webmaster@mbodm.com>
2022-02-26 18:55:07 +0100

    Removed all unnecessary usings, by clicking 'Remove and Sort Usings' in every .cs file.

--- WADM/MBODM.WADM/MBODM.ObservableModel/ObservableModel.cs
+++ WADM/MBODM.WADM/MBODM.ObservableModel/ObservableModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace MBODM
 {
--- WADM/MBODM.WADM/MBODM.WOW.CurseParser/Sourcecode/Public/Classes/CurseParserUsingApi.cs
+++ WADM/MBODM.WADM/MBODM.WOW.CurseParser/Sourcecode/Public/Classes/CurseParserUsingApi.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 
 namespace MBODM.WoW
 {
